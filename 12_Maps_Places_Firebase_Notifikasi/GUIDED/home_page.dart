031c111ab4b9dc0687966d730dec164792b0a16b
Gideon Ladiyo <alwaysfun0604@gmail.com>
2024-12-09 22:49:54 +0700

    update unguided, tp sabar

---
+++ 12_Maps_Places_Firebase_Notifikasi/GUIDED/home_page.dart
@@ -0,0 +1,68 @@
+// import 'package:flutter/material.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import 'package:place_picker/place_picker.dart';
+
+// class GoogleMapsView extends StatefulWidget {
+//   const GoogleMapsView({super.key});
+
+//   @override
+//   State<GoogleMapsView> createState() => _GoogleMapsViewState();
+// }
+
+// class _GoogleMapsViewState extends State<GoogleMapsView> {
+//   static final LatLng _kMapCenter = LatLng(-7.4305913, 109.2511863);
+//   static final CameraPosition _kInitialPosition = CameraPosition(
+//       target: _kMapCenter, zoom: 10.0, tilt: 0, bearing: 0);
+
+//   Set<Marker> _createMarker() {
+//     return {
+//       Marker(
+//         markerId: MarkerId("marker_1"),
+//         position: _kMapCenter,
+//         infoWindow: InfoWindow(title: "Marker 1"),
+//         rotation: 90,
+//       ),
+//       Marker(
+//         markerId: MarkerId("marker_2"),
+//         position: LatLng(-6.9733165, 107.6281415),
+//       ),
+//     };
+//   }
+
+//   void showPlacePicker() async {
+//     LocationResult result = await Navigator.of(context).push(
+//       MaterialPageRoute(
+//         builder: (context) => PlacePicker(
+//           "API KEY", // Ganti dengan API Key yang benar
+//           displayLocation: _kMapCenter, // Koordinat untuk memulai peta
+//         ),
+//       ),
+//     );
+//     if (result != null) {
+//       print("Selected location: ${result.latLng}");
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Google Maps Demo"),
+//         actions: [
+//           IconButton(
+//             icon: Icon(Icons.search),
+//             onPressed: showPlacePicker,
+//           ),
+//         ],
+//       ),
+//       body: GoogleMap(
+//         initialCameraPosition: _kInitialPosition,
+//         myLocationEnabled: true,
+//         markers: _createMarker(),
+//         onMapCreated: (controller) {
+//           // Perform additional setup if needed
+//         },
+//       ),
+//     );
+//   }
+// }
